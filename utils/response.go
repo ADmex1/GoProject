4deecc271a6e5082e0ea17f3d48fcb8a0f2afd88
Viktor <157784316+ADmex1@users.noreply.github.com>
2025-10-06 19:15:12 +0800

    Created status on utils folder, 4 status crated Ok,Created,Notfound,BadReq

--- utils/response.go
+++ utils/response.go
@@ -1,7 +1,46 @@
 package utils
 
+import "github.com/gofiber/fiber/v2"
+
 type response struct {
-	Status       string `json:"status"`
-	ResponseCode string `json:"ResponseCode"`
-	Message      string `json:"message"`
+	Status       string      `json:"status"`
+	ResponseCode int         `json:"ResponseCode"`
+	Message      string      `json:"message,omitempty"`
+	Data         interface{} `json:"data,omitempty"`
+	Error        string      `json:"error"`
+}
+
+func success(c *fiber.Ctx, Message string, data interface{}) error {
+	return c.Status(fiber.StatusOK).JSON(response{
+		Status:       "success",
+		ResponseCode: fiber.StatusOK,
+		Message:      Message,
+		Data:         data,
+	})
+}
+func created(c *fiber.Ctx, Message string, data interface{}) error {
+	return c.Status(fiber.StatusCreated).JSON(response{
+		Status:       "Created",
+		ResponseCode: fiber.StatusCreated,
+		Message:      Message,
+		Data:         data,
+	})
+}
+func BadReq(c *fiber.Ctx, Message string, data interface{}, err string) error {
+	return c.Status(fiber.StatusBadRequest).JSON(response{
+		Status:       "Bad Request",
+		ResponseCode: fiber.StatusBadRequest,
+		Message:      Message,
+		Data:         data,
+		Error:        err,
+	})
+}
+func NotFound(c *fiber.Ctx, Message string, data interface{}, err string) error {
+	return c.Status(fiber.StatusNotFound).JSON(response{
+		Status:       "Not Found",
+		ResponseCode: fiber.StatusNotFound,
+		Message:      Message,
+		Data:         data,
+		Error:        err,
+	})
 }
