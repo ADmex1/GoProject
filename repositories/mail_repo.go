376ff47eb1e41bf97b62fcb818016f096be271b4
Viktor <157784316+ADmex1@users.noreply.github.com>
2026-08-18 17:22:45 +0800

    Addred AddReceiver on both Repo and Service

--- repositories/mail_repo.go
+++ repositories/mail_repo.go
@@ -1,11 +1,21 @@
 package repositories
 
 import (
+	"time"
+
 	"github.com/ADMex1/GoProject/config"
 	"github.com/ADMex1/GoProject/models"
 )
 
 type MailRepository interface {
+	CreateMailThread(mailThread *models.MailThread) error
+	CreateMailReceiver(mailReceiver *models.MailReceiver) error
+	CreateMailAttachment(mailAttachment *models.MailAttachment) error
+	FindThreadByPublicID(publicID *string) (*models.MailThread, error)
+	FetchReceiverByThreadPublicID(mailThreadPublicID string) ([]models.User, error)
+	AddReceiver(mailThreadID uint, userIDs []uint) error
+	DeleteMailThread(id uint) error
+	CreateMail(mail *models.Mail) error
 }
 
 type MailRepositorys struct {
@@ -20,9 +30,6 @@ func (r *MailRepositorys) CreateMailThread(mailThread *models.MailThread) error 
 }
 func (r *MailRepositorys) CreateMailReceiver(mailReceiver *models.MailReceiver) error {
 	return config.DB.Create(mailReceiver).Error
-}
-func (r *MailRepositorys) CreateMail(mail *models.Mail) error {
-	return config.DB.Create(mail).Error
 }
 
 func (r *MailRepositorys) CreateMailAttachment(mailAttachment *models.MailAttachment) error {
@@ -41,8 +48,28 @@ func (r *MailRepositorys) FetchReceiverByThreadPublicID(mailThreadPublicID strin
 	return users, err
 }
 
+func (r *MailRepositorys) AddReceiver(mailThreadID uint, userIDs []uint) error {
+	if len(userIDs) == 0 {
+		return nil
+	}
+	now := time.Now()
+	var receiver []models.MailReceiver
+	for _, userIDs := range userIDs {
+		receiver = append(receiver, models.MailReceiver{
+			MailThreadID: int64(mailThreadID),
+			UserID:       int64(userIDs),
+			SentAt:       now,
+		})
+	}
+	return config.DB.Create(&receiver).Error
+}
+
 func (r *MailRepositorys) DeleteMailThread(id uint) error {
 	return config.DB.Delete(&models.MailThread{}, id).Error
+}
+
+func (r *MailRepositorys) CreateMail(mail *models.Mail) error {
+	return config.DB.Create(mail).Error
 }
 
 // func (r *MailRepositorys) FindMailThreadByID(id uint) (*models.MailThread, error) {
--- services/mail.go
+++ services/mail.go
@@ -1,11 +1,72 @@
 package services
 
+import (
+	"errors"
+
+	"github.com/ADMex1/GoProject/models"
+	"github.com/ADMex1/GoProject/repositories"
+	"github.com/google/uuid"
+)
+
 type MailService interface {
 }
 
 type MailServices struct {
+	userRepo repositories.UserRepository
+	mailRepo repositories.MailRepository
 }
 
 func NewMailService() MailService {
 	return &MailServices{}
 }
+
+// func (s *MailServices) CreateNewMailThread(mailThreadd models.Mail)error{
+
+// }
+
+func (s *MailServices) AddReceiver(mailThreadPublicID string, userPublicIDs []string) error {
+	mailThread, err := s.mailRepo.FindThreadByPublicID(&mailThreadPublicID)
+	if err != nil {
+		return errors.New("Mail Thread Not Found!")
+	}
+	var userInternalIDs []uint
+	for _, userPublicID := range userPublicIDs {
+		user, err := s.userRepo.FindByPublicID(userPublicID)
+		if err != nil {
+			return errors.New("User not found!")
+		}
+		userInternalIDs = append(userInternalIDs, uint(user.InternalID))
+	}
+	existingReceiver, err := s.mailRepo.FetchReceiverByThreadPublicID(string(mailThread.PublicID.String()))
+	if err != nil {
+		return err
+	}
+
+	receiverMap := make(map[uint]bool)
+	for _, receiver := range existingReceiver {
+		receiverMap[uint(receiver.InternalID)] = true
+	}
+
+	var NewReceiverIDs []uint
+	for _, userID := range userInternalIDs {
+		if !receiverMap[userID] {
+			NewReceiverIDs = append(NewReceiverIDs, userID)
+		}
+	}
+	if len(NewReceiverIDs) == 0 {
+		return nil
+	}
+	return s.mailRepo.AddReceiver(uint(mailThread.InternalID), NewReceiverIDs)
+}
+
+func (s *MailServices) CreateNewMail(mail models.Mail) error {
+	users, err := s.userRepo.FindByPublicID((mail.UserPubID).String())
+	if err != nil {
+		return errors.New("user not exist")
+	}
+	mail.PublicID = uuid.New()
+	mail.UserID = users.InternalID
+	return s.mailRepo.CreateMail(&mail)
+}
+
+// func(s *MailServices)
